559187bb5a612e7843a2e1280e2ffa37acbe5ce8
afaankhawaja <aafaan02@gmail.com>
2025-08-13 17:21:22 +0500

    added navbar

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Lato, Abril_Fatface } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+
+
+const lato = Lato({
+  variable: "--font-lato",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const abrilFatface = Abril_Fatface({
+  variable: "--font-abril-fatface",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Aura & Co",
+  description: "we sell everything",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={` ${lato.variable} ${abrilFatface.variable} antialiased`}
+      >
+        <div className="sticky top-0">
+        <Navbar/>
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { Search, ShoppingBag } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="py-4 px-10 shadow-md">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-x-12">
+          <h1 className="text-[#171412] text-[20px] leading-[23px] font-bold">
+            Aura & <span className="text-red-400">Co</span>
+          </h1>
+          <div className="ml-auto flex gap-x-4 text-[14px] text-[#171412] font-medium leading-[21px]">
+            <a href="/shop" className="hover:text-gray-600 px-2 py-2">
+              Shop
+            </a>
+            <a href="/about" className="hover:text-gray-600 px-2 py-2">
+              About
+            </a>
+            <a href="/contact" className="hover:text-gray-600 px-2 py-2">
+              Contact
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Search size={20} />
+          <ShoppingBag size={20} />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
